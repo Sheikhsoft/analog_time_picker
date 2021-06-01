5ab03e7ceacdbf6cc0f29d62f4555c57ab6a58e2
marwen <marwanbk2000@gmail.com>
2021-06-01 22:32:41 +0100

    refactor

--- lib/timepicker/clock_dial_painter.dart
+++ lib/timepicker/clock_dial_painter.dart
@@ -32,8 +32,8 @@ class ClockDialPainter extends CustomPainter {
   ];
 
   ClockDialPainter({this.clockText = ClockText.roman})
-      : tickPaint = new Paint(),
-        textPainter = new TextPainter(
+      : tickPaint = Paint(),
+        textPainter = TextPainter(
           textAlign: TextAlign.center,
           textDirection: TextDirection.rtl,
         ),
@@ -59,16 +59,16 @@ class ClockDialPainter extends CustomPainter {
       tickMarkLength = i % 5 == 0 ? hourTickMarkLength : minuteTickMarkLength;
       tickPaint.strokeWidth =
           i % 5 == 0 ? hourTickMarkWidth : minuteTickMarkWidth;
-      canvas.drawLine(new Offset(0.0, -radius),
-          new Offset(0.0, -radius + tickMarkLength), tickPaint);
+      canvas.drawLine(Offset(0.0, -radius),
+          Offset(0.0, -radius + tickMarkLength), tickPaint);
 
       //draw the text
       if (i % 5 == 0) {
         canvas.save();
         canvas.translate(0.0, -radius + 20.0);
 
-        textPainter.text = new TextSpan(
-          text: this.clockText == ClockText.roman
+        textPainter.text = TextSpan(
+          text: clockText == ClockText.roman
               ? '${romanNumeralList[i ~/ 5]}'
               : '${i == 0 ? 12 : i ~/ 5}',
           style: textStyle,
@@ -80,7 +80,7 @@ class ClockDialPainter extends CustomPainter {
         textPainter.layout();
 
         textPainter.paint(canvas,
-            new Offset(-(textPainter.width / 2), -(textPainter.height / 2)));
+            Offset(-(textPainter.width / 2), -(textPainter.height / 2)));
 
         canvas.restore();
       }
