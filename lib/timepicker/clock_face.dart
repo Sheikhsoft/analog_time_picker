bd0ae98c5e50e1ff338f2e70f97d9410ef9c9e26
marwen <marwanbk2000@gmail.com>
2021-06-01 22:33:26 +0100

    refactor

--- lib/timepicker/clock_face.dart
+++ lib/timepicker/clock_face.dart
@@ -15,41 +15,41 @@ class ClockFace extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return new Padding(
+    return Padding(
       padding: const EdgeInsets.all(10.0),
-      child: new AspectRatio(
+      child: AspectRatio(
         aspectRatio: 1.0,
-        child: new Container(
+        child: Container(
           width: double.infinity,
-          decoration: new BoxDecoration(
+          decoration: BoxDecoration(
             shape: BoxShape.circle,
             color: Colors.white,
           ),
-          child: new Stack(
+          child: Stack(
             children: <Widget>[
               //dial and numbers
-              new Container(
+              Container(
                 width: double.infinity,
                 height: double.infinity,
                 padding: const EdgeInsets.all(10.0),
-                child: new CustomPaint(
-                  painter: new ClockDialPainter(clockText: clockText),
+                child: CustomPaint(
+                  painter: ClockDialPainter(clockText: clockText),
                 ),
               ),
 
               //centerpoint
-              new Center(
-                child: new Container(
+              Center(
+                child: Container(
                   width: 15.0,
                   height: 15.0,
-                  decoration: new BoxDecoration(
+                  decoration: BoxDecoration(
                     shape: BoxShape.circle,
                     color: Colors.black,
                   ),
                 ),
               ),
 
-              new ClockHands(
+              ClockHands(
                   dateTime: dateTime,
                   showHourHandleHeartShape: showHourHandleHeartShape),
             ],
